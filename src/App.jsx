1a4a6c0a539254cf99fe2747c049b78e8f3f5c4e
HassanMunene <awanzihassan@gmail.com>
2025-03-13 15:35:48 +0300

    first one

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import ClassComponentExample from './components/classComponent'
+
+function App() {
+  return (
+    <>
+      <ClassComponentExample />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/classComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Component } from "react";
+
+class ClassComponentExample extends Component {
+    render () {
+        return <h1>This is a class component</h1>;
+    }
+}
+
+export default ClassComponentExample;
