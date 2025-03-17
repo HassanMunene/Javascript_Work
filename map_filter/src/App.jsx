5c75635975d902d3ecb71f88644377e9c5ebddd7
HassanMunene <awanzihassan@gmail.com>
2025-03-17 16:27:10 +0300

    map and filter in react

---
+++ map_filter/src/App.jsx
@@ -0,0 +1,50 @@
+import React, { use } from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { jsx } from 'react/jsx-runtime';
+
+function App() {
+  const [users, setUsers] = useState(null);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setUsers(json);
+      })
+      .catch((error) => console.log("errror: ", error));
+  }, [])
+
+  useEffect(() => {
+    console.log(users);
+  }, [users])
+
+  const filteredData = users.filter((user) => {
+    return user.name.toLowerCase().includes('a');
+  })
+
+  console.log(filteredData);
+  return (
+    <div className='App'>
+      <h1>Users:</h1>
+      {users ? (
+        users.map((user, index) => (
+          <div className='card' key={user + "_" + index}>
+            <div className='card-inner'>
+              <p>{user.username}</p>
+              <p>{user.email}</p>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div className='card'>
+          <p>No Users Found</p>
+        </div >
+      )
+      }
+    </div >
+  )
+}
+
+export default App
